22a6b554b29eb93c9f7f042c3f291462eb251459
Bachrul Uluum <uluum123@gmail.com>
2024-12-26 01:14:23 +0000

    Add main layout template with responsive design, scripts, and footer components

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,38 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles / Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    {{-- animate css --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-
-    {{-- masonry --}}
-    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
-
-    {{-- slick --}}
-    @vite(['resources/css/slick.css', 'resources/css/slick-theme.css'])
-
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-
-
-</head>
-
-<body class="font-sans antialiase">
-   <x-navbar />
-
-    <main id="hero" class="bg-transparent">
+@extends('layouts.main')
+ 
+@section('title', 'Home')
+@section('content')
         <div class="bg-primary-800 min-h-screen mb-10 flex items-center relative pt-20 ">
             <div class="grid z-10 max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 grid-cols-1 lg:py-16 lg:grid-cols-12">
                 <div class="mr-auto place-self-center lg:col-span-7 text-white">
@@ -141,7 +110,6 @@
                 </div>
             @endforeach
         </div>
-
         {{-- END features --}}
 
         {{-- Layanan Kami --}}
@@ -335,8 +303,6 @@
         {{-- END Klien Kami --}}
 
         {{-- Testimonial --}}
-
-
         <section id="testimonies" class="py-20">
             <div class="max-w-6xl mx-8 md:mx-10 lg:mx-20 xl:mx-auto">
                 <div
@@ -563,138 +529,4 @@
             </div>
         </div>
         {{-- END MAPS --}}
-    </main>
-
-    {{-- footer --}}
-    <footer class="bg-primary-500 rounded-lg shadow m-4">
-        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div class="sm:flex sm:items-center sm:justify-between">
-                {{-- follow us on --}}
-                <div class="flex mt-4 sm:justify-center sm:mt-0">
-                    <a href="#" class="text-gray-100 hover:text-gray-300 ms-5">
-                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            fill="currentColor" viewBox="0 0 16 16">
-                            <path
-                                d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z">
-                            </path>
-                        </svg>
-                        <span class="sr-only">Linkedin account</span>
-                    </a>
-                    <a href="#" class="text-gray-100 hover:text-gray-300  ms-5">
-                        <svg class="w-4 h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 448 512"><!--! Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. -->
-                            <path
-                                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
-                            </path>
-                        </svg>
-                        <span class="sr-only">Instagram</span>
-                    </a>
-                    <a href="#" class="text-gray-100 hover:text-gray-300  ms-5">
-                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24"
-                            fill="currentColor">
-                            <title>TikTok</title>
-                            <path
-                                d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z">
-                            </path>
-                        </svg>
-                        <span class="sr-only">TikTok page</span>
-                    </a>
-                    <a href="#" class="text-gray-100 hover:text-gray-300  ms-5">
-                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            fill="currentColor" viewBox="0 0 16 16">
-                            <path
-                                d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z">
-                            </path>
-                        </svg>
-                        <span class="sr-only">Youtube page</span>
-                    </a>
-                </div>
-                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-100 sm:mb-0 ">
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
-            <span class="block text-sm text-gray-100 sm:text-center ">
-                Copy right PT Atna Mandiri Sentosa {{ date('Y') }}
-            </span>
-        </div>
-    </footer>
-    {{-- end footer --}}
-
-    {{-- floatng button --}}
-<x-fab />
-    {{-- end FAB --}}
-
-    <script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('.slider-carousel').slick({
-                slidesToShow: 1,
-                lazyLoad: 'ondemand',
-                slidesToScroll: 1,
-                autoplay: true,
-                autoplaySpeed: 2000,
-                arrows: true,
-                // dots: true,
-            });
-
-            particlesJS.load("particle", "{{ asset('particles.json') }}", function() {
-                // console.log("callback - particles.js config loaded");
-            });
-
-            var grid = document.querySelector('#masonry-grid');
-
-            var msnry = new Masonry(grid, {
-                itemSelector: '.grid-item',
-                columnWidth: '.grid-sizer',
-                percentPosition: true
-            });
-
-            imagesLoaded(grid).on('progress', function() {
-                // layout Masonry after each image loads
-                msnry.layout();
-            });
-
-            const featuresElements = document.querySelectorAll('.features');
-            const observer = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    const element = entry.target;
-
-                    if (entry.isIntersecting) {
-                        // Add animation with a slight delay
-                        setTimeout(() => {
-                            element.classList.add('animate__swing');
-                        }, 200); // Adjust the delay as needed
-                    } else {
-                        // Remove the animation class when out of view to reset it
-                        element.classList.remove('animate__swing');
-                    }
-                });
-            }, {
-                threshold: 0.5 // Trigger when 50% of the element is in the viewport (adjustable)
-            });
-
-            // Observe each .features element individually
-            featuresElements.forEach(element => {
-                observer.observe(element);
-            });
-
-
-        });
-    </script>
-</body>
-
-</html>
+    @endsection
